f28a8afc1e8c0cd4cdda6ac62ba1e5a3ecd7a173
Ron Almog <ron8132@gmail.com>
2024-09-09 14:05:20 -0400

    use TranslationsProvider

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,18 +1,25 @@
 import Link from "next/link";
 import initTranslations from "../i18n";
-import local from "next/font/local";
+import TranslationsProvider from "@/components/TranslationsProvider";
 
 export default async function Home({ params: { locale } }) {
-  const { t } = await initTranslations(locale, ["home", "common"]);
+  const i18Namespaces = ["home", "common"];
+  const { t, resources } = await initTranslations(locale, i18Namespaces);
   const lng = "zz";
   return (
-    <div>
-      <h1>{t("title")}</h1>
-      <Link href={`/${lng}/about`}>{t("common:about_us")}</Link>
-      <br />
-      <Link href={`/${lng}/second`}>{t("to-second-page")}</Link>
-      <br />
-      <Link href={`/${lng}/client-page`}>{t("to-client-page")}</Link>
-    </div>
+    <TranslationsProvider
+      locale={locale}
+      resources={resources}
+      namespaces={i18Namespaces}
+    >
+      <div>
+        <h1>{t("title")}</h1>
+        <Link href={`/${lng}/about`}>{t("common:about_us")}</Link>
+        <br />
+        <Link href={`/${lng}/second`}>{t("to-second-page")}</Link>
+        <br />
+        <Link href={`/${lng}/client-page`}>{t("to-client-page")}</Link>
+      </div>
+    </TranslationsProvider>
   );
 }
